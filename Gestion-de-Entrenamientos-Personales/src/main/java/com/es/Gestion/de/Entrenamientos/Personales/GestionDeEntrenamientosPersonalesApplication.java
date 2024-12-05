1fcc9288148f251242b9ae299fcea37527458e26
Abraham Rodriguez <arm.developer00@gmail.com>
2024-12-05 10:59:57 +0100

    generate app spring boot

---
+++ Gestion-de-Entrenamientos-Personales/src/main/java/com/es/Gestion/de/Entrenamientos/Personales/GestionDeEntrenamientosPersonalesApplication.java
@@ -0,0 +1,13 @@
+package com.es.Gestion.de.Entrenamientos.Personales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeEntrenamientosPersonalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeEntrenamientosPersonalesApplication.class, args);
+	}
+
+}
---
+++ Gestion-de-Entrenamientos-Personales/src/test/java/com/es/Gestion/de/Entrenamientos/Personales/GestionDeEntrenamientosPersonalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.Gestion.de.Entrenamientos.Personales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEntrenamientosPersonalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
